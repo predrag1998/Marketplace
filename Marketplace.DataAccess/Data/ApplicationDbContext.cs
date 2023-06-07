36658182fbd7150b3462c2c4bb604c6bf6ece069
Predrag Spasovski <predrag-spasovski@hotmail.com>
2023-06-07 14:35:11 +0200

    Extend Identity User and Register

--- Marketplace.DataAccess/Data/ApplicationDbContext.cs
+++ Marketplace.DataAccess/Data/ApplicationDbContext.cs
@@ -14,6 +14,7 @@ namespace Marketplace.DataAccess.Data
         }
         public DbSet<Category> Categories { get; set; }
         public DbSet<Product> Products { get; set; }
+        public DbSet<ApplicationUser> ApplicationUsers { get; set; }
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
