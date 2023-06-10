8e644f131f174ab522bc706f10ce15de50b563ed
Predrag Spasovski <predrag-spasovski@hotmail.com>
2023-06-10 14:59:28 +0200

    Update login and register

--- Marketplace_Web/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ Marketplace_Web/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -108,6 +108,12 @@ namespace Marketplace_Web.Areas.Identity.Pages.Account
             public string? Role { get; set; }
             [ValidateNever]
             public IEnumerable<SelectListItem> Roles { get; set; }
+            [Required]
+            public string Name { get; set; }
+            public string? StreetAddress { get; set; }
+            public string? City { get; set; }
+            public string? Country { get; set; }
+            public string? PhoneNumber { get; set; }
         }
 
 
@@ -140,6 +146,12 @@ namespace Marketplace_Web.Areas.Identity.Pages.Account
 
                 await _userStore.SetUserNameAsync(user, Input.Email, CancellationToken.None);
                 await _emailStore.SetEmailAsync(user, Input.Email, CancellationToken.None);
+                user.Name = Input.Name;
+                user.PhoneNumber = Input.PhoneNumber;
+                user.StreetAdress = Input.StreetAddress;
+                user.City = Input.City;
+                user.Country = Input.Country;
+                
                 var result = await _userManager.CreateAsync(user, Input.Password);
 
                 if (result.Succeeded)
